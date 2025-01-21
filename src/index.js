c3b2a7786c707db07475364b82dc43ce2add6586
Ivan-Adaits <ioliverfunes@adaits.es>
2025-01-21 03:18:36 +0100

    feat: Added Conecting db

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config(); // 👈 Esto carga las variables de .env
+import connectDB from './db.js';
+import config from './config.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000; // 👈 Asegurar que PORT se carga bien
 
-// Middleware básico
-app.use(express.json());
+connectDB();
 
-app.get('/home', (req, res) => {
-  res.send(`Servidor funcionando en el puerto ${PORT} 🚀`);
-});
-
-// Iniciar servidor
-app.listen(PORT, () => {
+const PORT = config.app.PORT;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${PORT}...`);
 });
