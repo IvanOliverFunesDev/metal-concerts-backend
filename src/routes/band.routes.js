7f3c06154de61f28ff075023b7cf41c22783d6c9
Ivan-Adaits <ioliverfunes@adaits.es>
2025-02-28 02:02:02 +0100

    feat(bands): implement markSubscribeBands flow

--- src/routes/band.routes.js
+++ src/routes/band.routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { checkBandStatus } from '../middleware/check-role.middleware.js';
-import { authRequired } from '../middleware/validate-token.middleware.js';
+import { authOptional, authRequired } from '../middleware/validate-token.middleware.js';
 import { upload } from '../middleware/upload.middleware.js';
 import { getAllBandsController, getBandPublicProfileController, getPopularBandsController, getTopRatedBandsController, updateBandProfileController } from '../controllers/bands/band.controller.js';
 
 const router = Router();
 
-router.get('/', getAllBandsController);
-router.get('/popular', getPopularBandsController);
-router.get('/top-rated', getTopRatedBandsController);
-router.get('/:id', getBandPublicProfileController);
+router.get('/', authOptional, getAllBandsController);
+router.get('/popular', authOptional, getPopularBandsController);
+router.get('/top-rated', authOptional, getTopRatedBandsController);
+router.get('/:id', authOptional, getBandPublicProfileController);
 router.put('/update-profile', authRequired, checkBandStatus('approved'), upload.single('file'), updateBandProfileController);
 
 export default router;
